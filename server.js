ff247f9b2e736141249130bf9914f3a9c1d2cdb5
230660221002 <230660221002@student.unsap.ac.id>
2025-12-19 16:53:46 +0700

    Update server js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
